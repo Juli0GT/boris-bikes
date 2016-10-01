b511c1758f37fd3da66645bf3bad413cbe49d61b
Julio Gonzalez <julio.gteran@gmail.com>
2016-10-01 16:00:22 +0100

    broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,18 @@
 class Bike
+
+	def report_broken
+		@broken = true
+	end
+
+	def report_working
+		@working = true
+	end 
+
+	def broken?
+		@broken
+	end
+
 	def working?
-		true
+		@working
 	end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,16 @@
 require 'bike.rb'
 describe Bike do
+	subject(:bike) {described_class.new}
+
 	it {is_expected.to respond_to :working?}
+
+	it "can be reported as broken" do
+		bike.report_broken
+		expect(bike).to be_broken
+	end
+
+	it "can be reported as working" do
+		bike.report_working
+		expect(bike).to be_working
+	end
 end
